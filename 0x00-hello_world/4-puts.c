c541e333e7844ba9ae7b359e4df657e4fc2fe9c5
root <faithfulyat@gmail.com>
2023-06-17 02:19:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main- prints "Programming is like building a multilingual puzzle, followed by a new line."
+ */
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return(0);
+}
