8f98ff9b25ae06af292be717465bb748c233ebcc
root <faithfulyat@gmail.com>
2023-06-18 12:54:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - a program the alphabet in lowercase.
+ * Followed by a new line.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
