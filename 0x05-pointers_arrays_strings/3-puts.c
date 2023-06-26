bc8c113e2772040282a3b88d7819b9b60ec1c386
root <faithfulyat@gmail.com>
2023-06-26 08:56:57 -0700

    _puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts - prints a string
  * Followed by a new line
@@ -8,12 +8,5 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
-	
-	while (str[i])
-	{
-		_putcar(str[i]);
-		i++;
-	}
-	_putcar('\n');
+	puts(str);
 }
