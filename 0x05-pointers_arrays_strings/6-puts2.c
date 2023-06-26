ac542f913312a4ec71edde1ab7c52ce4aecbe846
root <faithfulyat@gmail.com>
2023-06-26 10:25:46 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts - function shouldprint only one character out of two
+ * puts2 - function shouldprint only one character out of two
  * starting with the first one
  * @str: input
  * Return: print
