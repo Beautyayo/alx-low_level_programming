cbca3a7902fc8a0e121faef8f1a6e4054576e860
root <faithfulyat@gmail.com>
2023-07-07 07:00:10 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if a character is a digit
+ * @c: The number to be checked
+ * Return: 1 for digit character or for any else
+ */
+
+int _isdigit(int n)
+{
+        if (n >= 48 && n <= 57)
+        {
+                return (1);
+        }
+        return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of the string
+ * @s: string to be checked
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+        int longi = 0;
+
+        while (*s != '\0')
+        {
+                longi++;
+                s++;
+        }
+        return (longi);
+}
