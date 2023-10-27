02a50654b767b242be045ef72ca92c33d627a757
Beautyayo <faithfuloyat@gmail.com>
2023-10-26 17:25:30 -0700

    #include"main.h"
    /**
     * get_bit - prog returns the value of a bit at a given index.
     * @n: number to check bits in
     * @index: index at which to check bit
     *
     * Return: value of the bit, or -1 if there is an error
     */
    int get_bit(unsigned long int n, unsigned int index)
    {
    unsigned long int divisor, check;
    
    if (index > (sizeof(unsigned long int) * 8 - 1))
    return (-1);
    divisor = 1 << index;
    check = n & divisor;
    if (check == divisor)
    return (1);
    return (0);
    }

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,25 +1,21 @@
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
 
 /**
-* get_bit - function that gets the value of int at index
-*
-* @n: pointer to number
-*
-* @index: index to change
-*
-* Return: 1 on success, -1 on failure
-*/
-
+ * get_bit - prog returns the value of a bit at a given index.
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ *
+ * Return: value of the bit, or -1 if there is an error
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bit_value, bin;
+	unsigned long int divisor, check;
 
-	if (index > (sizeof(unsigned long int) * 8))
-	return (-1);
-
-	bin = n >> index;
-
-	bit_value = bin & 1;
-	return (bit_value);
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	divisor = 1 << index;
+	check = n & divisor;
+	if (check == divisor)
+		return (1);
+	return (0);
 }
